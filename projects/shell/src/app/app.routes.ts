b9294e6d0982e6277a7ad8e1a1c62a04e0a01c43
Ivan Rozhon <ivan.rozhon@gmail.com>
2021-08-22 23:42:39 +0200

    dynamic module federation sample

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 
+import { loadRemoteModule } from '@angular-architects/module-federation';
+
 import { HomeComponent } from './home.component';
+
+const MF2_URL = 'http://localhost:4000/remoteEntry.js';
 
 export const routes: Routes = [
   {
@@ -10,5 +14,14 @@ export const routes: Routes = [
   {
     path: `search`,
     loadChildren: () => import('mf1/Module').then((m) => m.SearchModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      loadRemoteModule({
+        remoteEntry: MF2_URL,
+        remoteName: 'mf2',
+        exposedModule: './Module',
+      }).then((m) => m.AdminModule),
   },
 ];
